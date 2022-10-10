f6def48ac77ff5d3f9d140c595de55243fd9568b
OLGA <ochefranova@yandex.ru>
2022-10-11 00:04:10 +0500

    сделала д/з

--- Exercise13/Program.cs
+++ Exercise13/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+if  (num <= 99)
+{
+   Console.WriteLine("третьей цифры нет");
+}
+else if (num <= 999)
+{
+   Console.WriteLine($"третья цифра {num} -> {num % 10}"); 
+}
+else if (num <= 9999)
+{
+    Console.WriteLine($"третья цифра {num} -> {num / 10 % 10}");
+}
+else if (num <= 99999)
+{
+    Console.WriteLine($"третья цифра {num} -> {num / 100 % 10}");
+}
